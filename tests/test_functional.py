fb08cb937a9de3e1db53048af76f704630b38a54
x14119641 <daniel.gil.romero24@gmail.com>
2020-09-18 11:36:34 +0100

    Fixed the update_to_list test

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -41,26 +41,79 @@ def test_create_delete_db():
 
 
 def test_push_another_object_to_list_in_mongo():
+    mock_doc1 = {"id":"id1",
+                "my_dict_list": [{"name":"BATIRI RCA"}]
+                }
+    mock_doc2 = {"id":"id1",
+                "my_dict_list": [{"name":"BATIRI"}]
+                }
+    mock_doc3 = {"id":"id1",
+                "my_dict_list": [{"name":"BATIRI RI"}]
+                }
     test_db = mongo.test_db
-    test_db.test_collection2.insert_one({
-        'id':'id1',
-        'my_dict_list': [{"key1":"value1"}]
-        })
-
+    print(test_db)
+    test_db.test_collection2.drop()
+    test_db.test_collection2.insert_one(mock_doc1)
+    print(test_db)
     assert 'test_collection2' in test_db.list_collection_names()
     query = test_db.test_collection2.find_one({})
-    assert "value1" in query['my_dict_list'][0].get('key1')
+    print(query)
+    assert "BATIRI RCA" == query['my_dict_list'][0].get('name')
 
     # update list
-    to_update = {"key2":"value2"}
+    print(mock_doc2["my_dict_list"][0])
+    
     if test_db.test_collection2.find_one({"id":"id1"}):
         test_db.test_collection2.update(
             {"id":"id1"},
-            {"$push": {"my_dict_list":to_update}}
+            {"$push": {"my_dict_list":mock_doc2["my_dict_list"][0]}}
             )
     
     query = test_db.test_collection2.find_one({})
-    assert "value2" in query['my_dict_list'][1].get('key2')
+    print(query)
+    assert "BATIRI" == query['my_dict_list'][1].get('name')
+
+
+    # dont update list if element in array does exist
+    query_dict_doc = test_db.test_collection2.find({
+        "my_dict_list": { "$elemMatch": {"name":"BATIRI"}}
+    })
+    # query_dict_doc = test_db.test_collection2.find({
+    #     "id":"id1", "my_dict_list.name": {"$exists":"true"}
+    # })
+    # query_dict_doc = test_db.test_collection2.find({
+    #     "my_dict_list": {"$elemMatch": {"name":"BATIRI"}}
+    # })
+    query_dict_doc = test_db.test_collection2.find_one( {
+                            "my_dict_list": { "$elemMatch": { "name": "BATIRI" } }
+                            })
+   
+
+    if not query_dict_doc:
+         test_db.test_collection2.update(
+            {"id":mock_doc2["id"]},
+            {"$push": {"my_dict_list":mock_doc2["my_dict_list"][0]}}
+            )
+
+    query_total_docs = test_db.test_collection2.find_one({})
+    # Not update as dict exist in array
+    assert len(query_total_docs['my_dict_list']) ==2
+
+    # # update list if element does not exist
+    query_dict_doc = test_db.test_collection2.find_one( {
+                            "my_dict_list": { "$elemMatch": { "name": mock_doc3["my_dict_list"][0].get('name') } }
+                            })
+    print(query_dict_doc)
+
+    if not query_dict_doc:
+         test_db.test_collection2.update(
+            {"id":mock_doc3["id"]},
+            {"$push": {"my_dict_list":mock_doc3["my_dict_list"][0]}}
+            )
+
+    query_total_docs = test_db.test_collection2.find_one({})
+    # New document addaded
+    assert len(query_total_docs['my_dict_list']) ==3
 
     test_db.test_collection2.drop()
     assert 'test_collection' not in test_db.list_collection_names()
